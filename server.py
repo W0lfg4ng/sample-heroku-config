d5d924ddcb4e8be958517957a11732523ad18955
W0lfg4ng <31731516+W0lfg4ng@users.noreply.github.com>
2018-02-14 17:30:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import requests
 from handler import handle_message
 
 while True:
-    res = request.get("https://api.telegram.org/")
+    res = request.get("https://api.telegram.org/bot253609155:AAGCp5I5uJiwZkqkyCVEatH3b8vi3RYxuY8")
     d = res.json()
 
     for elem in d["result"]:
