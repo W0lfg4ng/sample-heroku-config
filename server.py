15c1bc50347ef35027c2fc9409d9fdaac6a132c4
W0lfg4ng <31731516+W0lfg4ng@users.noreply.github.com>
2018-03-14 16:59:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import requests
 from handler import handle_message
 
+last_update_id = 0
+
 while True:
-    res = requests.get("https://api.telegram.org/bot253609155:AAGCp5I5uJiwZkqkyCVEatH3b8vi3RYxuY8/getUpdates")
+    res = requests.get("https://api.telegram.org/bot253609155:AAGCp5I5uJiwZkqkyCVEatH3b8vi3RYxuY8/getUpdates", params={"offset": last_update_id})
     d = res.json()
 
     for elem in d["result"]:
         text = elem["message"]["text"]
+        
+        last_update_id = elem["update_id"] + 1
+        
         ans = handle_message(text)
 
         chat_id = elem["message"]["chat"]["id"]
