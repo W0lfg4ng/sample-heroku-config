837608f1af70bc55b1920e82f18d8343ce489f86
W0lfg4ng <31731516+W0lfg4ng@users.noreply.github.com>
2018-03-14 17:20:14 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,5 +1,69 @@
 from datetime import datetime
 
+lessons = [
+    [
+        {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "", "teacher": ""},
+        {"subject": "Французский язык", "teacher": "Архипова Д. В."},
+        {"subject": "", "teacher": ""},
+        {"subject": "Химия", "teacher": "Малютина Е. М."},
+        {"subject": "История", "teacher": "Авдеев А. Г."},
+        {"subject": "История", "teacher": "Авдеев А. Г."},
+        {"subject": "История", "teacher": "Авдеев А. Г."},
+        {"subject": "", "teacher": ""}
+    ],
+    [
+        {"subject": "", "teacher": ""},
+        {"subject": "Французский язык", "teacher": "Архипова Д. В."},
+        {"subject": "", "teacher": ""},
+        {"subject": "Алгебра", "teacher": ""},
+        {"subject": "Алгебра", "teacher": ""},
+        {"subject": "Биология", "teacher": "Черткова Е. Р."},
+        {"subject": "Биология", "teacher": "Черткова Е. Р."},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""}
+    ],
+    [
+        {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "Геометрия", "teacher": "Назарова К. А."},
+        {"subject": "Геометрия", "teacher": "Назарова К. А."},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""}
+    ],
+    [
+        {"subject": "", "teacher": ""},
+        {"subject": "Французский язык", "teacher": "Архипова Д. В."},
+        {"subject": "Химия", "teacher": "Малютина Е. М."},
+        {"subject": "Химия", "teacher": "Малютина Е. М."},
+        {"subject": "Алгебра", "teacher": ""},
+        {"subject": "Алгебра", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""}
+    ],
+    [
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "", "teacher": ""},
+        {"subject": "История", "teacher": "Авдеев А. Г."},
+        {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
+        {"subject": "Биология", "teacher": "Черткова Е. Р."},
+        {"subject": "Биология", "teacher": "Черткова Е. Р."}
+    ],
+    ["Выходной"],
+    ["Выходной"]
+]
 
 def hello():
     print('Здравствуйте')
@@ -15,68 +79,5 @@ def handle_message(text):
         print(day)
     else:
         return 'Введите дату в корректном формате'
-    lessons = [
-        [
-            {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "", "teacher": ""},
-            {"subject": "Французский язык", "teacher": "Архипова Д. В."},
-            {"subject": "", "teacher": ""},
-            {"subject": "Химия", "teacher": "Малютина Е. М."},
-            {"subject": "История", "teacher": "Авдеев А. Г."},
-            {"subject": "История", "teacher": "Авдеев А. Г."},
-            {"subject": "История", "teacher": "Авдеев А. Г."},
-            {"subject": "", "teacher": ""}
-        ],
-        [
-            {"subject": "", "teacher": ""},
-            {"subject": "Французский язык", "teacher": "Архипова Д. В."},
-            {"subject": "", "teacher": ""},
-            {"subject": "Алгебра", "teacher": ""},
-            {"subject": "Алгебра", "teacher": ""},
-            {"subject": "Биология", "teacher": "Черткова Е. Р."},
-            {"subject": "Биология", "teacher": "Черткова Е. Р."},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""}
-        ],
-        [
-            {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "Геометрия", "teacher": "Назарова К. А."},
-            {"subject": "Геометрия", "teacher": "Назарова К. А."},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""}
-        ],
-        [
-            {"subject": "", "teacher": ""},
-            {"subject": "Французский язык", "teacher": "Архипова Д. В."},
-            {"subject": "Химия", "teacher": "Малютина Е. М."},
-            {"subject": "Химия", "teacher": "Малютина Е. М."},
-            {"subject": "Алгебра", "teacher": ""},
-            {"subject": "Алгебра", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""}
-        ],
-        [
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "", "teacher": ""},
-            {"subject": "История", "teacher": "Авдеев А. Г."},
-            {"subject": "Литература", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "Русский язык", "teacher": "Угрехелидзе В. Г."},
-            {"subject": "Биология", "teacher": "Черткова Е. Р."},
-            {"subject": "Биология", "teacher": "Черткова Е. Р."}
-        ],
-        ["Выходной"],
-        ["Выходной"]
-    ]
+
     return lessons[day]
